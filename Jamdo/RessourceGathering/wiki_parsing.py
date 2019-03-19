5a52b583582b2d3bdc01eeef317a3071761502df
kothve <oliviermercierpeetz@live.ca>
2019-03-19 11:49:29 -0400

    Add files via upload

--- Jamdo/RessourceGathering/wiki_parsing.py
+++ Jamdo/RessourceGathering/wiki_parsing.py
@@ -23,7 +23,8 @@ def output_data(year, month, day, type):
           "&titles="+str(year)
 
     monthDict = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
-                 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+                 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December',
+                 13: 'Nobel Prizes'}
 
     month_name = monthDict[month]
     nextMonth = month+1
