89d4ac14b1d536ac3b4185b612f0d7943c7098b1
Andy Nguyen <brobromario@hotmail.com>
2019-04-12 13:07:04 -0400

    Update Authentication server from master

--- Jamdo/AuthenticationServer/auth.py
+++ Jamdo/AuthenticationServer/auth.py
@@ -5,12 +5,12 @@ import datetime
 from main import get_secret
 
 # JWT token - https://realpython.com/token-based-authentication-with-flask/
-def encode_auth_token(user_id):
+def encode_auth_token(sub):
 	try:
 		payload = {
 			'exp': datetime.datetime.utcnow() + datetime.timedelta(days=365, minutes=10), # ONE YEAR
 			'iat': datetime.datetime.utcnow(),
-			'sub': user_id
+			'sub': sub
 		}
 		return jwt.encode(
 			payload,
@@ -20,30 +20,22 @@ def encode_auth_token(user_id):
 	except Exception as e:
 		return e
 
-def decode_auth_token(auth_token):
+def decode_auth_token(token):
 	auth = {}
 	try:
-		payload = jwt.decode(auth_token, get_secret())
-		auth['payload'] = payload
+		payload = jwt.decode(token, get_secret())
+		auth['payload'] = payload['sub']		
+		auth['status'] = 'success'
+		auth['message'] = 'Authentication approved of server: ['+payload["sub"]+']'
 		return auth
 	except jwt.ExpiredSignatureError:
-		auth['message'] = 'Authentication token Expired. Please Log In.'
+		auth['message'] = 'Authentication token Expired'
+		auth['status'] = 'fail'
+		auth['payload'] = 'None'		
 		return auth
 	except jwt.InvalidTokenError:
-		auth['message'] = 'Authentication token invalid. Please Log In'
+		auth['status'] = 'fail'
+		auth['message'] = 'Authentication token invalid'
+		auth['payload'] = 'None'		
 		return auth
 
-# Decorator - checks token on required pages
-# https://stackoverflow.com/questions/34495632/how-to-implement-login-required-decorator-in-flask
-def protected_endpoint(f):
-	@wraps(f)
-	def wrapper(*args, **kws):
-		token = request.cookies.get('token');
-		decoded_token = {}
-		if not token:
-			decoded_token['message'] = 'Authentication Error: No token. Please Log In'
-			return f(decoded_token,*args,**kws)
-		else:
-			decoded_token = decode_auth_token(token)
-		return f(decoded_token,*args,**kws)
-	return wrapper
