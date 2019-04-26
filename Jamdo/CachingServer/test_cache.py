b7b9c7378170b18f61b8b862dd221f51e4f71bc7
jamese1394 <32024347+jamese1394@users.noreply.github.com>
2019-04-26 16:18:25 -0400

    Update test_cache.py

--- Jamdo/CachingServer/test_cache.py
+++ Jamdo/CachingServer/test_cache.py
@@ -26,7 +26,7 @@ class TestResult(unittest.TestCase):
         Result.query.delete()
         self.db.session.commit()
 
-    def test_get_all_results(self):
+    def test_dump_database(self):
         # send the request and check the response status code
         response = self.app.get("/showDatabase")
         self.assertEqual(response.status_code, 200)
@@ -37,7 +37,7 @@ class TestResult(unittest.TestCase):
         self.assertDictEqual(result_list [0], {"year": "1994", "event": "Alice"})
         self.assertDictEqual(result_list [1], {"year": "1995", "event": "Bob"})
 
-    def test_get_result_with_valid_year(self):
+    def test_get_result_year_with_valid_year(self):
         # send the request and check the response status code
         response = self.app.get("/isCached/birth/1994")
         self.assertEqual(response.status_code, 200)
@@ -46,16 +46,16 @@ class TestResult(unittest.TestCase):
         result = json.loads(str(response.data, "utf8"))
         self.assertDictEqual(result, {"year": "1994", "type": "birth", "event": "Alice"})
 
-    def test_get_result_with_invalid_year(self):
+    def test_get_event_year_with_invalid_year(self):
         # send the request and check the response status code
         response = self.app.get("/isCached/birth/2000")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 204)
 
         # convert the response data from json and call the asserts
         body = json.loads(str(response.data, "utf8"))
-        self.assertDictEqual(body, {"code": 404, "msg": "Cannot find this result."})
+        self.assertDictEqual(body, {"code": 204, "msg": "no results"})
 
-    def test_get_result_with_valid_month(self):
+    def test_get_event_month_with_valid_month(self):
         # send the request and check the response status code
         response = self.app.get("/isCached/birth/1996/1/")
         self.assertEqual(response.status_code, 200)
@@ -64,16 +64,16 @@ class TestResult(unittest.TestCase):
         result = json.loads(str(response.data, "utf8"))
         self.assertDictEqual(result, {"year": "1996", "month": "1", "type": "birth", "event": "John"})
 
-    def test_get_result_with_invalid_month(self):
+    def test_get_event_month_with_invalid_month(self):
         # send the request and check the response status code
         response = self.app.get("/isCached/birth/1996/5/")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 204)
 
         # convert the response data from json and call the asserts
         body = json.loads(str(response.data, "utf8"))
-        self.assertDictEqual(body, {"code": 404, "msg": "Cannot find this result."})
+        self.assertDictEqual(body, {"code": 204, "msg": "no results"})
 
-    def test_get_result_with_valid_day(self):
+    def test_get_event_day_with_valid_day(self):
         # send the request and check the response status code
         response = self.app.get("/isCached/birth/1997/2/1/")
         self.assertEqual(response.status_code, 200)
@@ -82,21 +82,30 @@ class TestResult(unittest.TestCase):
         result = json.loads(str(response.data, "utf8"))
         self.assertDictEqual(result, {"year": "1997", "month": "2", "day": "1", "type": "birth", "event": "Ann"})
 
-    def test_get_result_with_invalid_day(self):
+    def test_get_event_day_with_invalid_day(self):
         # send the request and check the response status code
         response = self.app.get("/isCached/birth/1997/2/2/")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 204)
 
         # convert the response data from json and call the asserts
         body = json.loads(str(response.data, "utf8"))
-        self.assertDictEqual(body, {"code": 404, "msg": "Cannot find this result."})
+        self.assertDictEqual(body, {"code": 204, "msg": "no results"})
 
-    def test_post_result_without_id(self):
+    def test_post_event_year_without_year(self):
+        # send the request and check the response status code
+        response = self.app.post("/isCached/birth//")
+        self.assertEqual(response.status_code, 403)
+
+        # convert the response data from json and call the asserts
+        body = json.loads(str(response.data, "utf8"))
+        self.assertDictEqual(body, {"code": 403, "msg": "Cannot post event. Missing mandatory fields."})
+
+    def test_post_event_year_with_year(self):
         # do we really need to check counts?
         initial_count = Result.query.filter_by(name="Dan").count()
 
         # send the request and check the response status code
-        response = self.app.post("/person", data={"name": "Dan"})
+        response = self.app.post("/isCached/birth/1990/")
         self.assertEqual(response.status_code, 200)
 
         # convert the response data from json and call the asserts
@@ -107,12 +116,21 @@ class TestResult(unittest.TestCase):
         updated_count = Result.query.filter_by(name="Dan").count()
         self.assertEqual(updated_count, initial_count + 1)
 
-    def test_post_person_new_id(self):
+    def test_post_event_month_without_year(self):
+        # send the request and check the response status code
+        response = self.app.post("/isCached/birth//1/")
+        self.assertEqual(response.status_code, 403)
+
+        # convert the response data from json and call the asserts
+        body = json.loads(str(response.data, "utf8"))
+        self.assertDictEqual(body, {"code": 403, "msg": "Cannot post event. Missing mandatory fields."})
+
+    def test_post_event_month_with_year(self):
         # do we really need to check counts?
         initial_count = Result.query.filter_by(name="Dan").count()
 
         # send the request and check the response status code
-        response = self.app.post("/person", data={"name": "Dan"})
+        response = self.app.post("/isCached/birth/1990/1/")
         self.assertEqual(response.status_code, 200)
 
         # convert the response data from json and call the asserts
@@ -123,12 +141,12 @@ class TestResult(unittest.TestCase):
         updated_count = Result.query.filter_by(name="Dan").count()
         self.assertEqual(updated_count, initial_count + 1)
 
-    def test_post_person_with_valid_id(self):
+    def test_post_event_day(self):
         # do we really need to check counts?
         initial_count = Result.query.filter_by(id="1").count()
 
         # send the request and check the response status code
-        response = self.app.post("/person", data={"id": "1", "name": "Jim"})
+        response = self.app.post("/isCached/birth/1990/1/1/")
         self.assertEqual(response.status_code, 200)
 
         # convert the response data from json and call the asserts
