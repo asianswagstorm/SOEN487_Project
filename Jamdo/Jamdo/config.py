c1141ed9b816b7e0232b5c0ca8ce8081bf077bc5
Andy Nguyen <brobromario@hotmail.com>
2019-04-26 23:20:17 -0400

    finished user test

--- Jamdo/Jamdo/config.py
+++ Jamdo/Jamdo/config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = r"sqlite:///soen487.sqlite"
+    #SQLALCHEMY_DATABASE_URI = r"sqlite:///test_Users.sqlite" #manual test user
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret_key'
     SERVER_AUTH_NAME = 'application'
     SERVER_AUTH_PASSWORD = 'APPLICATION_SERVER_PASSWORD'
@@ -24,4 +25,4 @@ class TestConfig(Config):
     TESTING = True
     SERVER_AUTH_NAME = 'application'
     SERVER_AUTH_PASSWORD = 'APPLICATION_SERVER_PASSWORD'
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///../tests/test_Users.sqlite"
+    SQLALCHEMY_DATABASE_URI = r"sqlite:///test_Users.sqlite"
--- Jamdo/Jamdo/test_user.py
+++ Jamdo/Jamdo/test_user.py
@@ -3,22 +3,24 @@ import json
 import sqlite3
 from routes import *
 from config import TestConfig
-from models import User, db
-from main import app as tested_app 
+from models import User, db as tested_db
+from main import app as tested_app , bcrypt 
 
+"""
+from routes import *
+from config import TestConfig
+from main import app as tested_app
+"""
 tested_app.config.from_object(TestConfig)
 
 class TestApp(unittest.TestCase):
     @classmethod
     def setUp(self):
-        self.app = tested_app.test_client()
-        self.main = tested_app.app_context()
-        with tested_app.app_context():
-            db.init_app(tested_app)  # removes cyclic dependency??
+       
+        self.db = tested_db 
         self.db.create_all()
-        self.db.session.add(User(id=1, fname="Alice",lname= "Jackson", uname= "Alice_J", password="password"))
-        self.db.session.add(User(id=2, fname="Bob",lname= "Builder", uname= "Bob_The_Builder", password="password"))
-
+        self.db.session.add(User(id=1, fname="Alice",lname= "Jackson", username= "Alice_J", password=bcrypt.generate_password_hash("password")))
+        self.db.session.add(User(id=2, fname="Bob",lname= "Builder", username= "Bob_The_Builder", password=bcrypt.generate_password_hash("password")))
         try:
          self.db.session.commit()
         except:
@@ -26,46 +28,47 @@ class TestApp(unittest.TestCase):
         finally:
          self.db.session.close() 
          self.app = tested_app.test_client()   
+         #self.main = tested_app.app_context()
 
     @classmethod
     def tearDownClass(self): #tearDown After each test method tearDownClass
-     
-     # clean up the DB after the tests
-     with tested_app.app_context():
-      User.query.delete() #This gives an error Doesn't WORK
-      self.db.session.commit()
-      self.db.session.close()    
-
+       # clean up the DB after the tests
+       User.query.delete() 
+       self.db.session.commit() 
+       self.db.session.close() 
+      # pass
     
-    def test_login_no_data(self):
+    #Problem with data input, unit test is not taking in data resulting in 422 error status
+    def testA1_login_no_data(self):
         response = self.app.post("/login", data= {"username" : "" , "password" : ""})
         self.assertEqual(response.status_code, 422)
 
-    def test_login_user_pass(self):
+    def testA2_login_user_pass(self):
         response = self.app.post("/login", data= {"username" : "Alice_J" , "password" : "password"})
         self.assertEqual(response.status_code, 200)
         
-    def test_login_user_incorrect_password(self):
+    def testA3_login_user_incorrect_password(self):
         response = self.app.post("/login", data= {"username" : "Bob_The_Builder" , "password" : "hahahhaha"})
         self.assertEqual(response.status_code, 401)
 
-    def test_login_username_not_exist(self):
+    def testA4_login_username_not_exist(self):
         response = self.app.post("/login", data= {"username" : "Random_Guy123" , "password" : "hahahhaha"})
         self.assertEqual(response.status_code, 404)   
-
-    def test_register_username_already_exist(self):
+    
+    def testA5_register_username_already_exist(self):
         response = self.app.post("/register", data= {"fname": "Alice" , "lname": "Jackson" ,"username" : "Alice_J" , "password" : "hahahhaha"})
         self.assertEqual(response.status_code, 500)   
-
-    def test_register_no_data(self):
+    
+    def testA6_register_no_data(self):
         response = self.app.post("/login", data= {"username" : "" , "password" : ""})
         self.assertEqual(response.status_code, 422)
-
-    def test_register_pass(self):
+    
+    def testA7_register_pass(self):
         response = self.app.post("/register", data= {"fname": "Jackie" , "lname": "Chan" ,"username" : "JackieChan" , "password" : "password"})
         self.assertEqual(response.status_code, 200)       
+    
+    def test_logout(self): 
 
-    def test_logout(self):
         response = self.app.get("/logout")
         self.assertEqual(response.status_code, 200)         
     
